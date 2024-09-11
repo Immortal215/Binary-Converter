183c6718c1d34d531cb6c682cf30fd6c3963c4bb
Immortal215 <sharulshah@icloud.com>
2024-09-11 08:22:49 -0500

    Initial Commit

---
+++ Binary Converter/Binary_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Binary_ConverterApp.swift
+//  Binary Converter
+//
+//  Created by Sharul M. Shah on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Binary_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
