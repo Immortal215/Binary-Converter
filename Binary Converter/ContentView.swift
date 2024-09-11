454d64fd057680b2c0f97e72340804e288159912
cchang6066 <162370353+cchang6066@users.noreply.github.com>
2024-09-11 08:42:19 -0500

    Update ContentView.swift

--- Binary Converter/ContentView.swift
+++ Binary Converter/ContentView.swift
@@ -1,101 +1,71 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var number: Int?
+        @State var int = 0
+    @State var test:[Int] = []
+    @State var test1 = 0
+    @State var answer:Double = 0
+    @State var c = 1
+    @State var subAnswer:Double = 0
 
     var body: some View {
         VStack {
-            TextField("Enter Number to Convert", value: $number, format: .number)
+            TextField("Enter Number to Convert", value: $int, format: .number)
                 .textFieldStyle(.roundedBorder)
                 .padding()
             
-            Text("In Binary that is\n\nUsing Radix : \(String(number ?? 0, radix: 2))\n\nUsing Recursive : \(solver2(num: number ?? 0))\n\nUsing Personal Function : \(solver(num: number ?? 0))")
-            
+            Text("In Binary that is\n\nUsing Radix : \(String(int ?? 0, radix: 2))\n\nUsing Personal Function : \(solver(num: int ?? 0))")
+            Button(action: {
+                            answer = 0
+                            subAnswer = 0
+                            c = 1
+                            test = binary(og: int)
+                            answer = pow(10.0, Double(test[0]))
+                            while true {
+                                if test.count > 1 {
+                                    subAnswer += pow(10.0, Double(test[c]))
+                                    c += 1
+                                }
+                                if c == test.count {
+                                    subAnswer = subAnswer/10
+                                    break
+                                }
+                            }
+                        }, label: {
+                            Text("Calculate")
+                                
+                        })
         }
         .multilineTextAlignment(.center)
         .foregroundStyle(.white)
     }
 }
-
-func solver2(num: Int) -> String {
-    if num == 0 {
-        return ""
-    }
-    
-    return solver2(num: num / 2) + String(num % 2)
-}
-
-func solver(num: Int) -> String {
-    var stringNum = ""
-    var number = num
-    var isEven = num % 2 == 0 ? true : false
-    var tempNum = 0
-    var powerOfTwoTimes = powerOfTwoTimes(number: num)
-    var counterNumber = false 
-    
-    if !isEven {
-        number -= 1 
-    }
-    
-    while powerOfTwoTimes > 0 {
-        tempNum = number - closestLowPowerOfTwo(number: number)
-        print("temp num\(tempNum)")
-        if tempNum > 0 {
-            stringNum += "1"
-            number -= closestLowPowerOfTwo(number: number)
-            powerOfTwoTimes -= 1 
-            if tempNum == 1 {
-                stringNum += "0"
-                number -= 1 
-                powerOfTwoTimes -= 1
-            }
-        } else if tempNum == 0 {
-            if pow(2.0, Double(powerOfTwoTimes)) == Double(closestLowPowerOfTwo(number: number)) {
-                if !counterNumber { 
-                    stringNum += "1"
-                    counterNumber = true 
-                    number = closestLowPowerOfTwo(number: number)
-                    //  print(stringNum)
-                } else {
-                    stringNum += "0"
-                }
-            } else {
-                stringNum += "0"
-            }
-            powerOfTwoTimes -= 1
-            // print(stringNum)
-        } else if num == 0 {
-            break
             
+
+func binary(og:Int) -> Array<Int>{
+    var array: [Int] = []
+    var v = og
+    while true {
+        var x = 1
+        var count = 0
+            while true {
+                x = x*2
+                count += 1
+                if x > v {
+                    array.append(count)
+                    x = x/2
+                    v = v - x
+                    break
+                }
+            }
+        if v <= 1 {
+            if v == 1 {
+                array.append(1)
+            }
+            array[0] = array[0] - 1
+            
+            break
         }
-        print("number : \(number)")
-    }   
-    print("string num : \(stringNum)")
-    
-    // 10 does not work
-    return "\(stringNum)" + "\(isEven ? "0" : "1")"
-}
-
-func closestLowPowerOfTwo(number: Int) -> Int {
-    if number < 3 { 
-        return 0
     }
-    
-    var lowerPower = 1
-    while lowerPower * 2 <= number {
-        lowerPower *= 2
-    }
-    print("lower power: \(lowerPower)")
-    return lowerPower
-}
-
-func powerOfTwoTimes(number: Int) -> Int {
-    var count = 0
-    var num = number 
-    while num > 1 {
-        count += 1 
-        num /= 2 
-    }
-    print("power of two count : \(count)")
-    return count 
+    return array
 }
