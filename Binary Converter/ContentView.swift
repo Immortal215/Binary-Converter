84d7c9b16925e618e8fecd081b8e6ac9a75dcbe1
Sharul Shah <sharulshah@icloud.com>
2024-09-13 09:21:05 -0500

    Update ContentView.swift

--- Binary Converter/ContentView.swift
+++ Binary Converter/ContentView.swift
@@ -7,6 +7,7 @@ struct ContentView: View {
     @State var answer:Double = 0
     @State var c = 1
     @State var subAnswer:Double = 0
+    @State var number : Int?
     var body: some View {
         
         VStack {
@@ -14,30 +15,36 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontDesign(.monospaced)
                 .padding()
+            TextField("Enter Number to Convert", value: $number, format: .number)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            
+            Text("In Binary that is\n\nUsing Radix : \(String(number ?? 0, radix: 2))\n\nUsing Recursive : \(solver2(number: number ?? 0))")
+                .multilineTextAlignment(.center)
             Text(String(format: "%.0f", answer + subAnswer))
             TextField("", value: $int, format: .number)
-                            .textFieldStyle(.roundedBorder)
-                            .frame(width:100)
-                        Button(action: {
-                            answer = 0
-                            subAnswer = 0
-                            c = 1
-                            test = binary(og: int)
-                            answer = pow(10.0, Double(test[0]))
-                            while true {
-                                if test.count > 1 {
-                                    subAnswer += pow(10.0, Double(test[c]))
-                                    c += 1
-                                }
-                                if c == test.count {
-                                    subAnswer = subAnswer/10
-                                    break
-                                }
-                            }
-                        }, label: {
-                            Text("Calculate")
-                                
-                        })
+                .textFieldStyle(.roundedBorder)
+                .frame(width:100)
+            Button(action: {
+                answer = 0
+                subAnswer = 0
+                c = 1
+                test = binary(og: int)
+                answer = pow(10.0, Double(test[0]))
+                while true {
+                    if test.count > 1 {
+                        subAnswer += pow(10.0, Double(test[c]))
+                        c += 1
+                    }
+                    if c == test.count {
+                        subAnswer = subAnswer/10
+                        break
+                    }
+                }
+            }, label: {
+                Text("Calculate")
+                
+            })
         }
     }
 }
@@ -47,16 +54,16 @@ func binary(og:Int) -> Array<Int>{
     while true {
         var x = 1
         var count = 0
-            while true {
-                x = x*2
-                count += 1
-                if x > v {
-                    array.append(count)
-                    x = x/2
-                    v = v - x
-                    break
-                }
+        while true {
+            x = x*2
+            count += 1
+            if x > v {
+                array.append(count)
+                x = x/2
+                v = v - x
+                break
             }
+        }
         if v <= 1 {
             if v == 1 {
                 array.append(1)
@@ -69,5 +76,10 @@ func binary(og:Int) -> Array<Int>{
     return array
 }
 //func secondBinary(og:Int, count: Int ) -> Int {
-//    
-
+//
+func solver2(number: Int) -> String {
+    if number == 0 {
+        return ""
+    }
+    return "\(solver2(number: number/2))\(number % 2)"
+}
